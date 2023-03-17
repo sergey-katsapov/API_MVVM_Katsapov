cd154aeb748568700fc731f3da8135621259d35b
Sergey Katsapov <katsapov.sergey@mail.ru>
2023-03-18 00:02:51 +0300

    [ADD] - simple ui main activity;

--- app/src/main/java/com/example/filmsmvvm/MainActivity.kt
+++ app/src/main/java/com/example/filmsmvvm/MainActivity.kt
@@ -1,24 +1,25 @@
 package com.example.filmsmvvm
 
 import android.os.Bundle
-import com.google.android.material.snackbar.Snackbar
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
-import androidx.navigation.ui.setupActionBarWithNavController
-import android.view.Menu
-import android.view.MenuItem
 import com.example.filmsmvvm.databinding.ActivityMainBinding
+import com.example.filmsmvvm.ui.FeedFragment
+import dagger.hilt.android.AndroidEntryPoint
 
+@AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityMainBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        if (savedInstanceState == null) {
+            val fragment = FeedFragment()
+            supportFragmentManager.beginTransaction()
+                .add(R.id.fragment_container, fragment)
+                .commit()
+        }
     }
 }
