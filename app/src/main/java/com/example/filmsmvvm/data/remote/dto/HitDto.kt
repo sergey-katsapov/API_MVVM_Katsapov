c95575b740e97e85f960192ef7796d70b715aa7c
Sergey Katsapov <katsapov.sergey@mail.ru>
2023-03-18 14:10:45 +0300

    [UPDATE] - hits dtos;

--- app/src/main/java/com/example/filmsmvvm/data/remote/dto/HitDto.kt
+++ app/src/main/java/com/example/filmsmvvm/data/remote/dto/HitDto.kt
@@ -1,9 +1,30 @@
 package com.example.filmsmvvm.data.remote.dto
 
+import com.example.filmsmvvm.domain.models.Hit
+import com.google.gson.annotations.SerializedName
+
 data class HitDto(
+    @SerializedName("id")
     val id: String,
+
+    @SerializedName("largeImageURL")
     val largeImageURL: String? = null,
+
+    @SerializedName("imageURL")
     val imageURL: String? = null,
+
+    @SerializedName("imageWidth")
     val imageWidth: Int? = null,
+
+    @SerializedName("imageHeight")
     val imageHeight: Int? = null
 )
+
+fun HitDto.toHit() =
+    Hit(
+        id = id,
+        largeImageURL = largeImageURL,
+        imageURL = imageURL,
+        imageWidth = imageWidth,
+        imageHeight = imageHeight
+    )
--- app/src/main/java/com/example/filmsmvvm/data/remote/dto/SearchHitsListDto.kt
+++ app/src/main/java/com/example/filmsmvvm/data/remote/dto/SearchHitsListDto.kt
@@ -1,7 +1,22 @@
 package com.example.filmsmvvm.data.remote.dto
 
+import com.example.filmsmvvm.domain.models.SearchHitsList
+import com.google.gson.annotations.SerializedName
+
 data class SearchHitsListDto(
+    @SerializedName("total")
     val total: Int? = null,
+
+    @SerializedName("totalHits")
     val totalHits: Int? = null,
+
+    @SerializedName("hits")
     val hits: List<HitDto>? = null
 )
+
+fun SearchHitsListDto.toSearchHitsList() =
+    SearchHitsList(
+        total = total,
+        totalHits = totalHits,
+        hits = hits?.map { it.toHit() }
+    )
