bdf1ea1e4d2719a488f245b8061eb3a916cda8b6
Sergey Katsapov <katsapov.sergey@mail.ru>
2023-03-19 22:26:04 +0300

    [ADD] - constants file;

---
+++ app/src/main/java/com/example/filmsmvvm/Constants.kt
@@ -0,0 +1,10 @@
+package com.example.filmsmvvm
+
+object Constants {
+    const val PLAYER = "Player"
+    const val CHANNEL_NAME = "Music Channel"
+    const val CHANNEL_ID = "1"
+    const val MUSIC_NOTIFICATION_ID = 1
+    const val VOLUME_MAX_VALUE = 100f
+    const val DEFAULT_QUERY = "android"
+}
